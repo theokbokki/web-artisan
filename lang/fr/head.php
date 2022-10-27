553922a66eb29a48f056cd8c8edda2d39f7b3b58
theoleonet <theo.leonet@student.hepl.be>
2022-10-27 12:01:25 +0200

    :art: Added main layout component

---
+++ lang/fr/head.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'description' => 'Le site du bachelier en web et multimedia de la Haute Ecole de La Province de Liège',
+    'keywords' => 'HEPL, web, design web, dévelopemment web, ecole, Liège',
+];
---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description", content="{{ __('head.description') }}">
+  <meta name="keywords", content="{{ __('head.keywords') }}">
+  <meta name="author", content="Théo Léonet">
+  <meta name="viewport",
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ config('app.name') }}</title>
+  @vite('resources/css/app.css');
+</head>
+
+<body>
+  {{ $slot }}
+</body>
+
+</html>
