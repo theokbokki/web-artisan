878e6f34a244327c0c16fbec802ef1c7457cc4ff
theoleonet <theo.leonet@student.hepl.be>
2022-10-29 20:33:02 +0200

    :lipstick: Added custom breakpoint for main nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				title: ['Sora', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-			}
+			},
+			screens: {
+				'main-nav': '850px',
+			},
 		},
 	},
 	plugins: [],
