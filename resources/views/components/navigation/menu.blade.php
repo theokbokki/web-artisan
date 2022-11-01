c4c01f061e1b3e133a080a0770f4fc4b015d502e
theoleonet <theo.leonet@student.hepl.be>
2022-11-01 18:15:45 +0100

    :sparkles: Added the menu for big devices

---
+++ resources/views/components/navigation/menu.blade.php
@@ -0,0 +1,60 @@
+<aside class="flex-1 hidden sticky top-6 sm:flex mt-24 w-52">
+  <h2 class="sr-only">Menu</h2>
+  <ul class="grid gap-y-4 max-h-[90vh] overflow-scroll w-full">
+    <li class=" w-full">
+      <a href="#" aria-current="page" class="menu-link">
+        Accueil
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Projets étudiants
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Grille des cours
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Professeurs
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Anciens
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Blog
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Forum
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Stages
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Traductions
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        S'inscrire à l'école
+      </a>
+    </li>
+    <li class=" w-full ">
+      <a href="#" class="menu-link">
+        Contact
+      </a>
+    </li>
+  </ul>
+</aside>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
 <x-layouts.main>
   <x-navigation.header />
+  <x-navigation.menu />
 </x-layouts.main>
