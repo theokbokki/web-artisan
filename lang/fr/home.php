527e3fdc4727de74b2b6703c3401f87237401b01
theoleonet <theo.leonet@student.hepl.be>
2022-11-03 09:20:44 +0100

    :sparkles: Added the introduction to the home page

---
+++ lang/fr/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'hero_image_alt' => 'Image du code de ce site',
+    'intro_cta' => 'Viens verifier par toi même',
+    'intro_p3' => 'Trop beau pour être vrai?',
+    'intro_p2' => 'Après 3 ans chez nous, tu seras un vrai développeur full stack, mais pas que.',
+    'intro_p1' => 'Notre bachelier te permettra de te lancer dans tous les domaines du web, du développement au design.',
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,29 @@
 <x-layouts.main>
   <x-navigation.header />
-  <x-navigation.menu />
+  <div class="flex gap-x-24">
+    <x-navigation.menu />
+    <main class="w-full mt-10 md:mt-24">
+      <section class="flex items-center">
+        <div class="sm:min-w-max">
+          <h2 class="text-5xl font-title font-bold max-w-[33rem]">N’attends pas le chômage, deviens développeur web</h2>
+          <div class="text-lg text-slate-500 space-y-4 mt-6 max-w-[27.5rem]">
+            <p class="">
+              {{ __('home.intro_p1') }}
+            </p>
+            <p class="">
+              {{ __('home.intro_p2') }}
+            </p>
+            <p class="">
+              {{ __('home.intro_p3') }}
+            </p>
+          </div>
+          <a href="#" class="button inline-block mt-8 max-w-max">
+            {{ __('home.intro_cta') }}
+          </a>
+        </div>
+        <img src="images/hero-image.png" alt="{{ __('home.hero_image_alt') }}"
+          class="relative drop-shadow-2xl rounded-lg hidden sm:block h-[34rem] min-w-[55rem] shrink-0 perspective-r -ml-24">
+      </section>
+    </main>
+  </div>
 </x-layouts.main>
