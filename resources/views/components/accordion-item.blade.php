de5bff74d4e7f648854c3f92132a18ac0a412ccf
theoleonet <theo.leonet@student.hepl.be>
2022-11-04 12:39:28 +0100

    :iphone: Made the teachings for mobile

--- resources/views/components/accordion-item.blade.php
+++ resources/views/components/accordion-item.blade.php
@@ -1,14 +1,14 @@
-<li class="group/item inline-block" {{ $attributes }}>
-  <div
-    class="xl:ml-[32rem] 2xl:ml-[42rem] group-hover/item:bg-slate-100 group-focus/item:bg-slate-100 cursor-pointer rounded-xl inline-block w-full max-w-[28rem] px-5 group-hover/item:py-5 group-focus/item:py-5 transition-all ease duration-300">
-    <p class="text-xl font-bold">{{ $title }}</p>
-    <div
-      class="text-slate-600 group-focus/item:mt-6 max-h-0 group-focus/item:max-h-screen overflow-hidden ease duration-150">
-      <p>{{ $content }}</p>
-    </div>
+<li
+  class="group inline-block hover:bg-slate-100 focus:bg-slate-100 hover:p-5 focus:p-5 transition-all ease-in-out duration-300 rounded-md focus:space-y-2"
+  {{ $attributes }}>
+  <div class="flex justify-between text-slate-700">
+    <p class="title text-xl">{{ $title }}</p>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="w-6 h-6 group-focus:rotate-90 transition-all duration-300">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+    </svg>
   </div>
-  <div class="absolute flex top-8 left-0 min-w-0">
-    <img src="{{ $imgSrc }}" alt="{{ $imgAlt }}"
-      class="hidden xl:inline-block drop-shadow-2xl min-w-0 max-w-[30rem] 2xl:max-w-[40rem] group-focus/item:opacity-100 opacity-0 transition duration-300 rounded-md perspective-l">
+  <div class="max-h-0 group-focus:max-h-screen overflow-hidden transition-all ease-in-out duration-150">
+    <p>{{ $content }}</p>
   </div>
 </li>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,38 @@
+@props([
+    'accordionItems' => [
+        1 => [
+            'title' => 'Front-end',
+            'content' => 'Le front-end, c’est la partie du développement que l’utilisateur va voir au final, ce qui s’affiche à l’écran.',
+            'imgSrc' => 'images/frontend.png',
+            'imgAlt' => '',
+        ],
+        2 => [
+            'title' => 'Back-end',
+            'content' => 'Le développement back-end sert à gérer la partie que l’utilisateur ne voit pas. La connection au server web, la gestion de formulaires, l’authentification d’utilisateurs, les bases de données, …',
+            'imgSrc' => 'images/backend.png',
+            'imgAlt' => '',
+        ],
+        3 => [
+            'title' => 'Design',
+            'content' => 'Pendant la formation, tu seras amené à créer plusieurs sites ainsi que des applications mobiles. La création passe bien sur par une phase de design. Tes professeurs t’encadreront afin de t’enseigner les principes importants du design, l’UX (user experience) ainsi que l’UI (user interface).',
+            'imgSrc' => 'images/design.png',
+            'imgAlt' => '',
+        ],
+        4 => [
+            'title' => 'Accessibilité',
+            'content' => 'Un point important de notre formation est l’accessibilité web. Nous pensons que n’importe qui doit pouvoir utiliser un site, peut importe ses capacités d’accessibilité. Tu devras donc apprendre les bonnes pratiques d’accessibilité afin de pouvoir créer des sites web résilients et adaptatifs.',
+            'imgSrc' => 'images/accesibility.png',
+            'imgAlt' => '',
+        ],
+        5 => [
+            'title' => 'Gestion de projets',
+            'content' => 'Dès la deuxième année, tu sera amené à créer tes propres projets, et il faudra apprendre à s’organiser pour les mener à bien. Tu apprendras le processus de création d’un site web du cahier des charges jusqu’à la mise en ligne.',
+            'imgSrc' => 'images/project-management.png',
+            'imgAlt' => '',
+        ],
+    ],
+])
+
 <x-layouts.main>
   <x-navigation.header />
   <section class="mt-16 space-y-4">
@@ -8,5 +43,15 @@
       <p>{{ __('home.intro_p3') }}</p>
     </div>
     <a class="button inline-block" href="#">{{ __('home.intro_cta') }}</a>
+  </section>
+  <section class="mt-16 space-y-4">
+    <h2 class="title">{{ __('home.teachings_title') }}</h2>
+    <p>{{ __('home.teachings_subtitle') }}</p>
+    <ul class="relative mt-8 space-y-4">
+      @foreach ($accordionItems as $accordionItem)
+        <x-accordion-item tabindex="0" :title="$accordionItem['title']" :content="$accordionItem['content']" :img-src="$accordionItem['imgSrc']" :img-alt="$accordionItem['imgAlt']" />
+      @endforeach
+    </ul>
+    <a href="#" class="button inline-block">{{ __('home.teachings_cta') }}</a>
   </section>
 </x-layouts.main>
