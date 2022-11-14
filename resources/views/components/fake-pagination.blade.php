ff388b3069c00c15fb1868b0a7ce945a4a557ea0
theoleonet <theo.leonet@student.hepl.be>
2022-11-14 10:18:36 +0100

    :sparkles: Added a fake pagination

---
+++ resources/views/components/fake-pagination.blade.php
@@ -0,0 +1,18 @@
+<div {{ $attributes->merge(['class' => 'flex flex-wrap items-center gap-2']) }}>
+  <a href="#" class="pagination-links">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+      class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+  </a>
+  <a href="#" class="button inline-block px-4 py-1">1</a>
+  <a href="#" class="hidden pagination-links py-1.5 px-4 sm:block">2</a>
+  <a href="#" class="hidden pagination-links py-1.5 px-4 sm:block">3</a>
+  <a href="#" class="hidden pagination-links py-1.5 px-4 sm:block">4</a>
+  <a href="#" class="pagination-links">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+    </svg>
+  </a>
+</div>
