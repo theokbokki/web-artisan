d10f3638f44372272b3883a5bd471f750d97511b
theoleonet <theo.leonet@student.hepl.be>
2023-01-04 20:11:27 +0100

    :lipstick: Finished the blog show page ui

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,12 +1,15 @@
 <x-layouts.main>
   <article class="w-[90%] max-w-5xl mx-auto space-y-6">
-    <img src="/{{ $post->thumbnail }}" alt="" class="w-full aspect-video object-cover">
+    <div>
+      <img src="/{{ $post->thumbnail }}" alt="" class="w-full aspect-video object-cover rounded-md">
+    </div>
     <h2 class="small-title">{{ $post->title }}</h2>
     <div class="space-y-4">
       {!! $post->body !!}
     </div>
   </article>
-  <section>
+  <section class="w-[90%] max-w-5xl mx-auto mt-24 space-y-8">
+    <h2 class="small-title">{{ __('D\'autres posts que vous pourriez aimer') }}</h2>
     @foreach ($randomPosts as $post)
       <x-blog.post-card :$post />
     @endforeach
